1de523363d70312b80595453e20d8e95e24ad879
PonjevicDino <75270990+PonjevicDino@users.noreply.github.com>
2025-05-09 11:37:03 +0200

    Modified TileMerger

--- Assets/Scripts/Editor/TileMerger.cs
+++ Assets/Scripts/Editor/TileMerger.cs
@@ -3,13 +3,16 @@ using UnityEditor;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
+using UnityEditor.Formats.Fbx.Exporter;
+using System.Runtime.Serialization;
 
 public class TileMerger : EditorWindow
 {
+    private readonly string ExportFolder = "Assets/ExportedMergedFBX/";
     private string folderPath = "Assets";
     private string filterByAsset = "";
     private float radiusToMergeBy = 25f;
+    private ExportModelOptions exportModelOptions = new ExportModelOptions();
     [MenuItem("Tools/Merge Tiles")]
     static void Init()
     {
@@ -21,12 +24,13 @@ public class TileMerger : EditorWindow
     {
         GUILayout.Label("FBX + Texture Importer & Merger", EditorStyles.boldLabel);
 
-        filterByAsset = EditorGUILayout.TextField("Filter by Asset", "21");
-        radiusToMergeBy = EditorGUILayout.FloatField("Filter by Raidus", 25f);
-
+        filterByAsset = EditorGUILayout.TextField("Filter/Split by Asset Name", filterByAsset);
+        radiusToMergeBy = EditorGUILayout.FloatField("Merge Radius", radiusToMergeBy);
+        EditorGUILayout.LabelField("Folder Path: " + folderPath);
         if (GUILayout.Button("Select Folder"))
         {
-            folderPath = EditorUtility.OpenFolderPanel("Select Tile Folder", folderPath, "");
+            folderPath = EditorUtility.OpenFolderPanel("Select FBX Folder", folderPath, "");
+
         }
 
         if (GUILayout.Button("Import and Apply Textures"))
@@ -34,12 +38,25 @@ public class TileMerger : EditorWindow
             MergeTiles();
         }
     }
+    string GetUnityRelativePath(string fullPath)
+    {
+        string projectPath = UnityEngine.Application.dataPath.Replace("\\", "/");
+        fullPath = fullPath.Replace("\\", "/");
+
+        if (fullPath.StartsWith(projectPath))
+        {
+            // Add "Assets" back since Application.dataPath ends in "/Assets"
+            return "Assets" + fullPath.Substring(projectPath.Length);
+        }
+
+        return null;
+    }
     public void MergeTiles()
     {
         // Prompt for folder
         if (string.IsNullOrEmpty(folderPath)) return;
         // Get all FBX files
-        EditorUtility.DisplayProgressBar("TileMerger", "Filtering FBX Files", 0);
+        EditorUtility.DisplayProgressBar("FBX Merger", "Filtering FBX Files", 0);
         //EditorUtility.DisplayProgressBar("Importing FBX Files");
         var fbxFiles = Directory.EnumerateFiles(folderPath, "*.fbx");
         var used = new HashSet<string>();
@@ -56,16 +73,15 @@ public class TileMerger : EditorWindow
         foreach (string fbxPath in fbxFiles)
         {
             EditorUtility.DisplayProgressBar("Importing FBX Files", $"Processing {Path.GetFileName(fbxPath)}", (float)count / totalFiles);
-            string absolutePath = fbxPath
-                .Replace("\\", "/")
-                .Split(new[] { "Assets/" }, System.StringSplitOptions.None)[2];
-            absolutePath = "Assets/" + absolutePath;
-            string texPath = Path.ChangeExtension(absolutePath.Split(".")[0] + "_Texture.fbx", ".jpg");
-            Texture2D tex = AssetDatabase.LoadAssetAtPath<Texture2D>(texPath);
+            string relativeUnityPath = GetUnityRelativePath(fbxPath);
+
+            Texture2D tex = null;
+            string texPath = Path.ChangeExtension(relativeUnityPath.Split(".")[0] + "_Texture.fbx", ".jpg");
+            tex = AssetDatabase.LoadAssetAtPath<Texture2D>(texPath);
             if (tex == null) { Debug.LogWarning($"Missing texture: {texPath}"); continue; }
 
-            GameObject model = AssetDatabase.LoadAssetAtPath<GameObject>(absolutePath);
-            if (model == null) { Debug.LogWarning($"Missing FBX: {absolutePath}"); continue; }
+            GameObject model = AssetDatabase.LoadAssetAtPath<GameObject>(relativeUnityPath);
+            if (model == null) { Debug.LogWarning($"Missing FBX: {relativeUnityPath}"); continue; }
             GameObject inst = PrefabUtility.InstantiatePrefab(model) as GameObject;
 
             // Apply texture
@@ -75,6 +91,8 @@ public class TileMerger : EditorWindow
             Material newMat = new Material(Shader.Find("HDRP/Lit"));
             newMat.SetTexture("_BaseColorMap", tex);
             renderer.sharedMaterial = newMat;
+
+
             // Add to list for grouping
             MeshFilter mf = inst.GetComponentInChildren<MeshFilter>();
             if (mf != null)
@@ -143,10 +161,11 @@ public class TileMerger : EditorWindow
 
                 // Combine into one mesh
                 Mesh combinedMesh = new Mesh();
+                combinedMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
                 combinedMesh.CombineMeshes(combineInstances.ToArray(), false, true);
 
                 // Create new GameObject for the combined mesh
-                GameObject merged = new GameObject(filterByAsset + "-MergedTile");
+                GameObject merged = new GameObject(filterByAsset + "-MergedTile-" + processedCount);
                 MeshFilter newMf = merged.AddComponent<MeshFilter>();
                 newMf.sharedMesh = combinedMesh;
                 MeshRenderer newMr = merged.AddComponent<MeshRenderer>();
@@ -168,132 +187,136 @@ public class TileMerger : EditorWindow
         finally
         {
             EditorUtility.ClearProgressBar();
+            count = 0;
+            totalFiles = mergedTiles.Count;
+            if (mergedTiles != null && mergedTiles.Count > 0)
+            {
+                EnsureExportDirectory();
+                foreach (GameObject go in mergedTiles)
+                {
+                    EditorUtility.DisplayProgressBar("Exporting Merged FBX Files", $"Processing {go}", (float)count / totalFiles);
+                    ExportGameObjectToFbx(go);
+                    count++;
+                }
+                Debug.Log($"Merged tiles into {mergedTiles.Count} combined objects.");
+            }
+            AssetDatabase.Refresh();
+
+        }
+        EditorUtility.ClearProgressBar();
+        Debug.Log("Finished Process");
+    }
+    private void ExportGameObjectToFbx(GameObject go)
+    {
+        string safeName = SanitizeFileName(go.name);
+        string folderPath = Path.Combine(ExportFolder + filterByAsset + "_FBXFolderAssets/");
+        string fbxPath = Path.Combine(folderPath, $"{safeName}.fbx");
+
+        EnsureExportDirectory();
+        if (File.Exists(fbxPath))
+        {
+            Object.DestroyImmediate(go);
+            return;
         }
 
-        Debug.Log($"Merged tiles into {mergedTiles.Count} combined objects.");
+        GameObject temp = go;
+        temp.name = safeName;
 
+        // Set export options
+        exportModelOptions.ExportFormat = ExportFormat.Binary;
+        exportModelOptions.KeepInstances = true;
+        exportModelOptions.ExportUnrendered = false;
+        exportModelOptions.EmbedTextures = false;
 
+        // Export without modifying materials
+        ModelExporter.ExportObject(fbxPath, temp, exportModelOptions);
 
-
-
-        // Step 2: Group into 2x2 based on transform positions
-        // Build a lookup of grid coordinates to tile
-        /*var coordMap = new Dictionary<Vector2Int, GameObject>();
-        float tileSize = 25.0f; // assume unit grid (adjust as needed)
-        float tolerance = 2.0f;
-
-        Vector2Int SnapToGrid(Vector3 pos)
-        {
-            int x = Mathf.RoundToInt(pos.x / tileSize);
-            int y = Mathf.RoundToInt(pos.z / tileSize);
-            return new Vector2Int(x, y);
-        }
-        *//*
-        foreach (GameObject tile in tiles)
-        {
-            Vector3 pos = tile.transform.GetChild(0).transform.position;
-            Vector2Int gridPos = SnapToGrid(pos);
-            if (!coordMap.ContainsKey(gridPos))
-                coordMap[gridPos] = tile;
-            else
-                Debug.LogWarning($"Duplicate tile at snapped grid position {gridPos}");
-            /*
-            Vector3 pos = tile.transform.GetChild(0).transform.position;
-            Vector2Int key = new Vector2Int(Mathf.RoundToInt(pos.x / tileSize), Mathf.RoundToInt(pos.z / tileSize));
-            coordMap[key] = tile;*/
-        //}
-        /*
-        // Iterate to find 2x2 squares
-        foreach (var kv in coordMap)
-        {
-            Vector2Int c = kv.Key;
-            if (kv.Value != null && used.Contains(kv.Value.name)) continue;
-            // Check right, up, up-right neighbors
-            Vector2Int right = new Vector2Int(c.x + 1, c.y);
-            Vector2Int up = new Vector2Int(c.x, c.y + 1);
-            Vector2Int upRight = new Vector2Int(c.x + 1, c.y + 1);
-            if (!coordMap.ContainsKey(right) || !coordMap.ContainsKey(up) || !coordMap.ContainsKey(upRight))
-                continue;
-            var tileA = coordMap[c];
-            var tileB = coordMap[right];
-            var tileC = coordMap[up];
-            var tileD = coordMap[upRight];
-            if (!tileA|| used.Contains(tileA.name) || used.Contains(tileB.name) ||
-                used.Contains(tileC.name) || used.Contains(tileD.name))
-                continue;
-
-            // Mark as used
-            used.Add(tileA.name); used.Add(tileB.name);
-            used.Add(tileC.name); used.Add(tileD.name);
-
-            // Merge these four
-            GameObject parent = new GameObject("MergedTile");
-            MeshFilter mf = parent.AddComponent<MeshFilter>();
-            MeshRenderer mr = parent.AddComponent<MeshRenderer>();
-
-            // Pack textures into atlas
-            /*
-            Texture2D[] texs = { DuplicateTexture(tileA.GetComponentInChildren<Renderer>().sharedMaterial.mainTexture as Texture2D),
-                                 DuplicateTexture(tileB.GetComponentInChildren<Renderer>().sharedMaterial.mainTexture as Texture2D),
-                                 DuplicateTexture(tileC.GetComponentInChildren<Renderer>().sharedMaterial.mainTexture as Texture2D),
-                                 DuplicateTexture(tileD.GetComponentInChildren<Renderer>().sharedMaterial.mainTexture as Texture2D) };
-            Texture2D atlas = new Texture2D(2048, 2048);
-            
-            Rect[] rects = atlas.PackTextures(texs, 2, 2048,false);*/
-        /*  Material[] materials = new Material[4];
-          materials[0] = tileA.GetComponentInChildren<Renderer>().sharedMaterial;
-          materials[1] = tileB.GetComponentInChildren<Renderer>().sharedMaterial;
-          materials[2] = tileC.GetComponentInChildren<Renderer>().sharedMaterial;
-          materials[3] = tileD.GetComponentInChildren<Renderer>().sharedMaterial;
-
-          // Combine meshes
-          var combines = new CombineInstance[4];
-          GameObject[] group = { tileA, tileB, tileC, tileD };
-          for (int i = 0; i < 4; i++)
-          {
-              var cf = group[i].GetComponentInChildren<MeshFilter>();
-              combines[i].mesh = cf.sharedMesh;
-              combines[i].transform = group[i].transform.GetChild(0).transform.localToWorldMatrix;
-          }
-          mf.sharedMesh = new Mesh();
-          mf.sharedMesh.CombineMeshes(combines, false, true);
-
-          mr.sharedMaterials = materials;
-          // Adjust UVs
-          /*
-          Vector2[] uvs = mf.sharedMesh.uv;
-          for (int i = 0; i < 4; i++)
-          {
-              Rect r = rects[i];
-              // For simplicity assume each mesh has non-overlapping vertex index ranges of equal length
-              int uvStart = i * (uvs.Length / 4);
-              int count = uvs.Length / 4;
-              for (int j = uvStart; j < uvStart + count; j++)
-              {
-                  Vector2 uv = uvs[j];
-                  uv.x = r.x + uv.x * r.width;
-                  uv.y = r.y + uv.y * r.height;
-                  uvs[j] = uv;
-              }
-          }
-          mf.sharedMesh.uv = uvs;*/
-
-        // Create new material with atlas
-        /*Material newMat = new Material(tileA.GetComponentInChildren<Renderer>().sharedMaterial);
-        newMat.mainTexture = atlas;
-        mr.sharedMaterial = newMat;*/
-
-
-
-        // Destroy original tiles
-        /*foreach (GameObject t in group)
-        {
-            Undo.DestroyObjectImmediate(t);
-        }
-    }*/
-
+        Object.DestroyImmediate(temp);
     }
 
+    private void ExportMaterialsTextures(Renderer renderer, string folderPath, string baseName)
+    {
+        if (renderer == null || renderer.sharedMaterials == null) return;
+
+        for (int i = 0; i < renderer.sharedMaterials.Length; i++)
+        {
+            var mat = renderer.sharedMaterials[i];
+            if (mat == null) continue;
+
+            Texture2D texture = mat.mainTexture as Texture2D;
+            if (texture == null) continue;
+
+            string texFileName = $"{baseName}_Texture{i}.jpg";
+            string texPath = Path.Combine(folderPath, texFileName);
+
+            SaveTextureToFile(texture, texPath);
+        }
+    }
+    private void SaveTextureToFile(Texture2D texture, string path)
+    {
+        RenderTexture rt = RenderTexture.GetTemporary(texture.width, texture.height, 0, RenderTextureFormat.ARGB32);
+        Graphics.Blit(texture, rt);
+        RenderTexture.active = rt;
+
+        Texture2D readableTex = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
+        readableTex.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
+        readableTex.Apply();
+
+        byte[] bytes = readableTex.EncodeToJPG(90);
+        File.WriteAllBytes(path, bytes);
+
+        RenderTexture.ReleaseTemporary(rt);
+        RenderTexture.active = null;
+        Object.DestroyImmediate(readableTex);
+    }
+    private void ExportMainTexture(GameObject go, string folderPath, string baseName)
+    {
+        Renderer renderer = go.GetComponentInChildren<Renderer>();
+        if (renderer == null || renderer.sharedMaterial == null) return;
+        Debug.Log("BEFORE TEXTURE BYLAT");
+        Texture2D texture = renderer.sharedMaterial.mainTexture as Texture2D;
+        if (texture == null) return;
+        Debug.Log("Not here blyat");
+
+        string texturePath = Path.Combine(folderPath, $"{baseName}_Texture.jpg");
+
+        // Create readable copy
+        RenderTexture rt = RenderTexture.GetTemporary(texture.width, texture.height);
+        Graphics.Blit(texture, rt);
+        RenderTexture.active = rt;
+
+        Texture2D readable = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
+        readable.ReadPixels(new Rect(0, 0, texture.width, texture.height), 0, 0);
+        readable.Apply();
+        RenderTexture.ReleaseTemporary(rt);
+        RenderTexture.active = null;
+
+        File.WriteAllBytes(texturePath, readable.EncodeToJPG(90));
+        Object.DestroyImmediate(readable);
+    }
+    private void EnsureExportDirectory()
+    {
+        if (!Directory.Exists(ExportFolder))
+        {
+            Directory.CreateDirectory(ExportFolder);
+            AssetDatabase.Refresh();
+        }
+        string targetFolder = Path.Combine(ExportFolder, filterByAsset + "_FBXFolderAssets/");
+        if (!Directory.Exists(targetFolder))
+        {
+            Directory.CreateDirectory(targetFolder);
+            AssetDatabase.Refresh();
+        }
+    }
+
+    private string SanitizeFileName(string name)
+    {
+        foreach (char c in Path.GetInvalidFileNameChars())
+        {
+            name = name.Replace(c, '_');
+        }
+        return name;
+    }
     private Texture2D DuplicateTexture(Texture2D source)
     {
         RenderTexture renderTex = RenderTexture.GetTemporary(
